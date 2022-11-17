0bd44fbfd4b38e91f78a4cb1eb4de46260075e3b
Ankush Pandey <ap539813@gmail.com>
2022-11-17 15:25:16 +0530

    added paths to different models

---
+++ assets.py
@@ -0,0 +1,4 @@
+logistic_regression = 'saved_models/LogisticRegression'
+decision_tree = 'saved_models/DecisionTreeClassifier'
+standerdscaler = 'saved_models/StandardScaler'
+onehotencoder = 'saved_models/OneHotEncoder'
