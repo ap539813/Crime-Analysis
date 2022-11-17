bcf4e3889cdff7c601162a2a2c5fb3c9e1c8affa
Ankush Pandey <ap539813@gmail.com>
2022-11-17 12:42:20 +0530

    Initial commit to create the structure of the repository

---
+++ important_variables.py
@@ -0,0 +1,23 @@
+import base64
+
+# Define the hight and width of the images as required
+
+img_width, img_height = 256,256
+
+input_shape = (img_width, img_height)
+
+
+theme_image_name = 'assets/Crime_homepage.png'
+
+"""### gif from local file"""
+file_ = open(theme_image_name, "rb")
+contents = file_.read()
+data_url = base64.b64encode(contents).decode("utf-8")
+file_.close()
+
+
+css_file_path = 'style/style.css'
+
+model_path_unet = 'assets/fracture_segmentation_unet_model.hdf5'
+model_path_encoder_decoder = 'assets/fracture_detection_encoder_decoder_model.hdf5'
+
